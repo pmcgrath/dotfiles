a930a94215a91fe08d47831e2dc1628889ea2626
pmcgrath <pmcgrath@gmail.com>
2019-09-29 15:44:04 +0100

    Updated manage plugins script

--- managevimplugins.sh
+++ managevimplugins.sh
@@ -5,16 +5,10 @@ source_directory_path=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 setup-vim-plugins() {
 	cd $source_directory_path
 
-	# Pathogen
-	mkdir -p _vim/autoload
-	curl -LSso _vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-
-	# Plugins
 	# Based on http://blogs.atlassian.com/2013/05/alternatives-to-git-submodule-git-subtree/
-	git subtree add --prefix _vim/bundle/ctrlp.vim https://github.com/kien/ctrlp.vim.git master --squash
-	git subtree add --prefix _vim/bundle/nerdtree https://github.com/scrooloose/nerdtree.git master --squash
-	git subtree add --prefix _vim/bundle/neocomplete.vim https://github.com/shougo/neocomplete.vim.git master --squash
-	git subtree add --prefix _vim/bundle/vim-go https://github.com/fatih/vim-go.git master --squash
+	git subtree add --prefix _vim/pack/plugins/start/ctrlp.vim https://github.com/ctrlpvim/ctrlp.vim master --squash
+	git subtree add --prefix _vim/pack/plugins/start/nerdtree https://github.com/scrooloose/nerdtree.git master --squash
+	git subtree add --prefix _vim/pack/plugins/start/vim-go https://github.com/fatih/vim-go.git master --squash
 }
 
 update-vim-plugins() {
@@ -26,17 +20,11 @@ update-vim-plugins() {
 	# Also every time there is a change it looks like you will need a commit - even if there appaears to be no difference - use: git diff-index HEAD
 	local -r update_branch=upd-vim-plugin
 	git checkout -b $update_branch	
-	
-	# Pathogen
-	curl -LSso _vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-        [ "$(git diff-index HEAD)" != "" ] && git add . 
-	
-	# Plugins
+
 	# Based on http://blogs.atlassian.com/2013/05/alternatives-to-git-submodule-git-subtree/
-	git subtree pull --prefix _vim/bundle/ctrlp.vim https://github.com/kien/ctrlp.vim.git master --squash
-	git subtree pull --prefix _vim/bundle/nerdtree https://github.com/scrooloose/nerdtree.git master --squash
-	git subtree pull --prefix _vim/bundle/neocomplete.vim https://github.com/shougo/neocomplete.vim.git master --squash
-	git subtree pull --prefix _vim/bundle/vim-go https://github.com/fatih/vim-go.git master --squash
+	git subtree pull --prefix _vim/pack/plugins/start/ctrlp.vim https://github.com/ctrlpvim/ctrlp.vim master --squash
+	git subtree pull --prefix _vim/pack/plugins/start/nerdtree https://github.com/scrooloose/nerdtree.git master --squash
+	git subtree pull --prefix _vim/pack/plugins/start/vim-go https://github.com/fatih/vim-go.git master --squash
 
 	git add .
 	git commit -m 'Updated vim plugins'
